df48c084e5a32fa0f4093ac63b4a77d36cfa104b
gilcu2 <gilcu2@gmail.com>
2016-10-30 19:14:37 +0100

    buttons painted

---
+++ app/src/main/java/DrawingView.kt
@@ -0,0 +1,18 @@
+package com.gilcu2.drawkotlin
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by gilcu2 on 10/30/16.
+ */
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    init {
+        setupDrawing()
+    }
+
+    fun setupDrawing() = {
+
+    }
+}
--- app/src/main/java/com/gilcu2/drawkotlin/MainActivity.kt
+++ app/src/main/java/com/gilcu2/drawkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gilcu2.drawkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,18 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 
+        val buttonsImage = listOf(R.drawable.new_pic, R.drawable.brush, R.drawable.eraser, R.drawable.save)
+
         verticalLayout {
-            padding = dip(30)
-            editText {
-                hint = "Name"
-                textSize = 24f
-            }
-            editText {
-                hint = "Password"
-                textSize = 24f
-            }
-            button("Login") {
-                textSize = 26f
+            linearLayout {
+                for (r in buttonsImage)
+                    imageButton {
+                        contentDescription = "@string/start_new"
+                        imageResource = r
+                    }
+            }.lparams {
+                height = dip(50)
+                gravity = Gravity.CENTER
             }
         }
     }
