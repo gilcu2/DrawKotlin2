6c0051c92b78f08b775997fd0e831b5255b1f184
gilcu2 <gilcu2@gmail.com>
2016-10-31 08:50:02 +0100

    color palette began

--- app/src/main/java/DrawingView.kt
+++ app/src/main/java/DrawingView.kt
@@ -3,11 +3,13 @@ package com.gilcu2.drawkotlin
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewManager
+import org.jetbrains.anko.custom.ankoView
 
 /**
  * Created by gilcu2 on 10/30/16.
  */
-class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+class DrawingView(context: Context) : View(context) {
     init {
         setupDrawing()
     }
@@ -16,3 +18,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
     }
 }
+
+public inline fun ViewManager.drawingView(theme: Int = 0) = drawingView(theme) {}
+public inline fun ViewManager.drawingView(theme: Int = 0, init: DrawingView.() -> Unit) =
+        ankoView({ DrawingView(it) }, theme, init)
--- app/src/main/java/com/gilcu2/drawkotlin/MainActivity.kt
+++ app/src/main/java/com/gilcu2/drawkotlin/MainActivity.kt
@@ -9,21 +9,55 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
-        val buttonsImage = listOf(R.drawable.new_pic, R.drawable.brush, R.drawable.eraser, R.drawable.save)
+        data class MainButtonData(val image: Int, val description: String)
+
+        val activityButtonsData = listOf(
+                MainButtonData(R.drawable.new_pic, "@string/start_new"),
+                MainButtonData(R.drawable.brush, "@string/brush"),
+                MainButtonData(R.drawable.eraser, "@string/eraser"),
+                MainButtonData(R.drawable.save, "@string/save"))
+
+        val colorsData = listOf(android.R.color.holo_blue_light, android.R.color.holo_red_light,
+                android.R.color.holo_green_light)
 
         verticalLayout {
+
+            // Activity selection
             linearLayout {
-                for (r in buttonsImage)
+
+                for (r in activityButtonsData)
                     imageButton {
-                        contentDescription = "@string/start_new"
-                        imageResource = r
+                        contentDescription = r.description
+                        imageResource = r.image
+                    }.lparams {
+                        height = matchParent
                     }
+
             }.lparams {
                 height = dip(50)
                 gravity = Gravity.CENTER
             }
+
+            drawingView {
+                backgroundColor = R.color.colorBackground.opaque
+            }.lparams {
+                margin = dip(5)
+            }
+
+            // Color selection
+            linearLayout {
+
+                colorsData.forEach {
+                    imageButton {
+                        backgroundColor = it.opaque
+                    }.lparams {
+                        height = matchParent
+                    }
+                }
+
+            }
+
         }
     }
 }
