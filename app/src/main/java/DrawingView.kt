27f1efdc23b465ac429681ee690c99cf9d409b27
gilcu2 <gilcu2@gmail.com>
2016-10-31 19:56:11 +0100

    begin events

--- app/src/main/java/DrawingView.kt
+++ app/src/main/java/DrawingView.kt
@@ -6,6 +6,12 @@ import android.view.View
 import android.view.ViewManager
 import org.jetbrains.anko.custom.ankoView
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
+
 /**
  * Created by gilcu2 on 10/30/16.
  */
@@ -14,8 +20,26 @@ class DrawingView(context: Context) : View(context) {
         setupDrawing()
     }
 
-    fun setupDrawing() = {
+    val drawPath = Path()
+    val drawPaint = Paint()
+    var paintColor = 0xFF6600
+    val canvasPaint = Paint(Paint.DITHER_FLAG)
 
+
+    fun setupDrawing() = {
+        drawPaint.setColor(paintColor)
+        drawPaint.setAntiAlias(true)
+        drawPaint.setStrokeWidth(20.0f)
+        drawPaint.setStyle(Paint.Style.STROKE)
+        drawPaint.setStrokeJoin(Paint.Join.ROUND)
+        drawPaint.setStrokeCap(Paint.Cap.ROUND)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas (canvasBitmap)
     }
 }
 
